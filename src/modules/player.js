d98610738fba95ea0d0f669c749e0b2336b75c28
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-08-11 17:36:42 +0700

    Add random attack function

--- src/modules/player.js
+++ src/modules/player.js
@@ -8,6 +8,14 @@ class Player {
     let y = coordinate[1];
     gameboard.receiveAttack(x, y);
   }
+
+  randomAttack(gameboard) {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+
+    if (!this.attack([x, y], gameboard)) this.randomAttack();
+    else this.attack([x, y], gameboard);
+  }
 }
 
 export default Player;

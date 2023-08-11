a7546760b0d3a91333c7ff52d2da275bea955199
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-08-11 13:44:56 +0700

    Add data attribute to the grid

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -2,10 +2,14 @@ const boards = document.querySelectorAll('.board');
 
 const displayBoardGrid = () => {
   boards.forEach(item => {
-    for (let i = 0; i < 100; i++) {
-      const boardGrid = document.createElement('div');
-      boardGrid.className = 'border border-solid border-black';
-      item.appendChild(boardGrid);
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        const boardGrid = document.createElement('div');
+        boardGrid.className = 'border border-solid border-black';
+        boardGrid.setAttribute('data-row', x);
+        boardGrid.setAttribute('data-col', y);
+        item.appendChild(boardGrid);
+      }
     }
   })
 }
